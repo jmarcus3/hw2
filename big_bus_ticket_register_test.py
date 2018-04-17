26ff4a862b1641f9358b7349da3d565446bac364
jmarcus3 <jmarcus3@uchicago.edu>
2018-04-16 22:34:09 -0500

    red

--- big_bus_ticket_register_test.py
+++ big_bus_ticket_register_test.py
@@ -6,5 +6,9 @@ import datetime
 class RegisterTest(unittest.TestCase):
     seller = bbtrc.Seller()
 
+    def test_sell_count_input(self):
+        today = datetime.date.today()
+        sold_message = self.seller.sell('j', today, 'blue')
+        self.assertEqual("Number of tickets must be an integer", sold_message)
 
 unittest.main()
